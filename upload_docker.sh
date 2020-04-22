42925ae5f739739a08b00fa2a3df7dbc1cf4c87c
[rakesh-kadam] <[rakeshkadam26498@gmail.com]>
2020-04-22 18:16:36 +0530

    Docker Upload Script

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="rakeshkadam/flaskapp"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username rakeshkadam
+docker tag flaskapp $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
