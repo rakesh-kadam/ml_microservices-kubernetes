5fdebf6cec6cf36ec35bf91727fc2e21450c22c5
[rakesh-kadam] <[rakeshkadam26498@gmail.com]>
2020-04-23 00:06:53 +0530

    final

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath='rakeshkadam/flaskapp'
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskapp --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/flaskapp 8000:80 
 
+# Get logs
+kubectl logs `kubectl get pods -o=name`
